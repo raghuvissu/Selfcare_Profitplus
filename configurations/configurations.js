dc270b4f8227d7bd54199d1d2bfb51039dfae11a
Raghu Chiluka <raghuchiluka111@gmail.com>
2020-08-21 08:45:22 +0530

    remoed all files

--- configurations/configurations.js
+++
@@ -1,4 +0,0 @@
-angular.module('configurations', [])
-.constant('TENANT','default')
-.constant('API_VERSION',selfcareModels.OBS_URL)
-.constant('CONTENT_TYPE','application/json; charset=utf-8');
--- directives/OnBlurDirective.js
+++
@@ -1,9 +0,0 @@
-selfcareApp.directive("ngOnblur",function($parse){
-            return function(scope, elm, attrs){
-                var onBlurFunction = $parse(attrs['ngOnblur']);
-                elm.bind("blur", function(event) {
-                    scope.$apply(function() {
-                        onBlurFunction(scope, { $event: event });
-                    });});
-            };
-});
